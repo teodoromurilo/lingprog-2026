982542f0990dd27c627ea53f04e9d5dcfd25256e
MURILO DA SILVA TEODORO <teodoromurilo49@gmail.com>
2026-02-23 10:37:50 -0300

    imc

---
+++ introducaojava/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // variavel do tipo String
+        String nome = "Murilo";
+        //variavel do tipo int
+        int idade = 37;
+        // variavel do tipo float
+        float peso= 98.5f;
+        float altura = 1.85f ;
+        // calcular o imc
+        float imc = peso/ (altura * altura);
+        System.out.println(nome+ " seu IMC é c " +imc);
+    }
+}
