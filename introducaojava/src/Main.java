5043cdf2b64f321e6952785330e338806222e966
Murilo Teodoro <teodoromurilo49@gmail.com>
2026-03-01 11:46:19 -0300

    imc final

--- introducaojava/src/Main.java
+++ introducaojava/src/Main.java
@@ -1,19 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
-        // variavel do tipo String
-        String nome = "Murilo";
-        //variavel do tipo int
-        int idade = 37;
-        // variavel do tipo float
-        float peso= 98.5f;
-        float altura = 1.85f ;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Imforme o seu nome");
+        String nome = entrada.next();
+        System.out.println("Imforme a sua idade");
+        int idade = entrada.nextInt();
+        System.out.println("Imforme o seu peso ");
+        float peso = entrada.nextFloat();
+        System.out.println("Imforme a sua altura");
+        float altura = entrada.nextFloat();
         // calcular o imc
-        float imc = peso/ (altura * altura);
-        System.out.println(nome+ " seu IMC é c " +imc);
+        float imc = peso / (altura * altura);
+        String mensagem = "";
         if (imc < 18.5f) {
             System.out.println("Você esta abaixo do peso");
+        } else if (imc >= 18.5f && imc <= 24.9f) {
+            mensagem = "Peso normal ";
+        } else if (imc >= 25 && imc <= 29.9f) {
+            mensagem = "Sobre peso";
+        } else if (imc > 30 && imc <= 34.9f) {
+            mensagem = "Obesidade grau 1 ";
+        } else if (imc >= 35 && imc <= 39.9f) {
+            mensagem = "Obesidade grau 2 ";
+        } else if (imc > 40) {
+            mensagem = "Obesidade grau 3";
+        } else
+            mensagem = "Valores invalidos";
+        {
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Peso: " + peso);
+            System.out.println("Altura: " + altura);
+            System.out.println("IMC: " + imc);
+            System.out.println("Mensagem: " + mensagem);
+
+
+
         }
     }
 }
