ce3cbef7768348993c08c2e3deb09a40001516ee
Your Name <teodoromurilo49@gamil.com>
2026-03-06 10:03:35 -0300

    JavaLanches

--- introducaojava/contaenergia/src/Main.java
+++ introducaojava/contaenergia/src/Main.java
@@ -1,47 +1,42 @@
-import java.util.Scanner;
+import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Informe o seu nome :");
-        String name = sc.nextLine();
-        System.out.println("Informe o valor da Leitura anterior  :");
-        int leitura_anterior = sc.nextInt();
-        System.out.println("Informe o valor da Leitura atual  :");
-        int leitura_atual = sc.nextInt();
-        float valorkwh = 0.60f;
-        String mensagem ="";
-        // Calcular o consumo do mês //
-        int consumo_mes = (leitura_atual - leitura_anterior);
-        float valor_bruto = consumo_mes * valorkwh;
-        // Aplicar bandeira tarifaria //
-        String bandeira = "";
-        float adicional = 0 ;
-        if (consumo_mes > 0 && consumo_mes <= 100f) {
-            bandeira = "verde";
-            adicional = 0 ;
-        } else if (consumo_mes > 100f && consumo_mes <= 200f) {
-            bandeira = "amarela ";
-            adicional = 2 ;
-        } else if (consumo_mes > 201f && consumo_mes <= 300f) {
-            bandeira = "vermelha";
-            adicional = 5;
-        } else if (consumo_mes > 300f) {
-            bandeira = "preta";
-            adicional = 10;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Imforme o seu nome");
+        String nome = entrada.next();
+        System.out.println("Imforme a sua idade");
+        int idade = entrada.nextInt();
+        System.out.println("Imforme o seu peso ");
+        float peso = entrada.nextFloat();
+        System.out.println("Imforme a sua altura");
+        float altura = entrada.nextFloat();
+        // calcular o imc
+        float imc = peso / (altura * altura);
+        String mensagem = "";
+        if (imc < 18.5f) {
+            System.out.println("Você esta abaixo do peso");
+        } else if (imc >= 18.5f && imc <= 24.9f) {
+            mensagem = "Peso normal ";
+        } else if (imc >= 25 && imc <= 29.9f) {
+            mensagem = "Sobre peso";
+        } else if (imc > 30 && imc <= 34.9f) {
+            mensagem = "Obesidade grau 1 ";
+        } else if (imc >= 35 && imc <= 39.9f) {
+            mensagem = "Obesidade grau 2 ";
+        } else if (imc > 40) {
+            mensagem = "Obesidade grau 3";
         } else
             mensagem = "Valores invalidos";
         {
-            float valor_final = valor_bruto + adicional;
-            System.out.println("\n--- RESUMO DA CONTA ---");
-            System.out.println("Cliente: " + name);
-            System.out.println("Consumo total: " + consumo_mes + " kWh");
-            System.out.println("Bandeira tarifária: " + bandeira);
-            System.out.printf("Valor final a pagar: R$ %.2f \n", valor_final);
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Peso: " + peso);
+            System.out.println("Altura: " + altura);
+            System.out.println("IMC: " + imc);
+            System.out.println("Mensagem: " + mensagem);
 
-            sc.close();
+
 
         }
-
     }
-
 }
